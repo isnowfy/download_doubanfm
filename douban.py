076d77682acd9a2d6f7ec95a530ed92244963e40
isnowfy <isnowfy@gmail.com>
2012-05-24 22:38:49 +0000

    add mp3 tag

--- douban.py
+++ douban.py
@@ -1,61 +1,84 @@
 # coding: utf-8
  
 from BeautifulSoup import BeautifulSoup
-import urllib,urllib2,cookielib,re
+import urllib, urllib2, cookielib, re, json, eyeD3, os
+
+num_p = re.compile(r'(\d+)')
+songs_dir = 'songs'
+base_url = 'http://douban.fm/j/mine/playlist?type=n&h=&channel=0&context=channel:0|subject_id:%s'
+
+def get_songs_information(url):
+    subject_id = num_p.search(url).groups()[0]
+    ret = json.loads(urllib2.urlopen(base_url % subject_id).read())
+    return filter(lambda x:x['album'].endswith(subject_id+'/'), ret['song'])
+
+def download(song):
+    try:
+        os.mkdir(songs_dir)
+    except:
+        pass
+    filename = '%s.mp3' % song['title']
+    filepath = os.path.join(songs_dir, filename)
+    if os.path.exists(filepath):
+        return
+    urllib.urlretrieve(song['url'], filepath)
+    picname = song['picture'][1+song['picture'].rindex('/'):]
+    picpath = os.path.join(songs_dir, picname)
+    urllib.urlretrieve(song['picture'], picpath)
+    tag = eyeD3.Tag()
+    tag.link(filepath)
+    tag.header.setVersion(eyeD3.ID3_V2_3)
+    tag.encoding = '\x01'
+    tag.setTitle(song['title'])
+    tag.setAlbum(song['albumtitle'])
+    tag.setArtist(song['artist'])
+    tag.setDate(song['public_time'])
+    tag.addImage(3, picpath)
+    os.remove(picpath)
+    tag.update()	
+
+def html_decode(html):
+    #return html.replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>').replace('&quot;', '"').replace('&#39;', "'")
+    import HTMLParser
+    return HTMLParser.HTMLParser().unescape(html)
  
-def handle(s):
-    return s.replace("&lt;","<").replace("&gt;",">").replace("\\","")
-   
-def fhandle(s):
-    return s.replace("/","and").replace(" ","")
- 
-def get(myurl,cookie):
-    url2="http://douban.fm/j/mine/playlist?type=n&h=&channel=0&context=channel:0|subject_id:%d"
+def get(myurl, cookie):
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
     urllib2.install_opener(opener)
-    req=urllib2.Request(myurl)
-    req.add_header('User-Agent','Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)')
-    req.add_header('Cookie',cookie)
-    content=urllib2.urlopen(req).read()
-    soup=BeautifulSoup(str(content))
-    soup=soup.find("div", { "id" : "record_viewer" })
-    for div in soup.findAll("div", { "class" : "info_wrapper" }):
-      div=div.find("div",{ "class" : "song_info" })
-      a=div.contents[1]
-      p1,p2,p3=div.contents[3].findAll("p")
-      print a["href"]+"\nsong:"+handle(p1.string)+"\nsinger:"+handle(p2.string)+"\nalbum:"+handle(p3.a.string)
-      p=re.compile(r'(\d+)')
-      m=p.search(a["href"])
-      num=int(m.groups()[0])
-      url3=url2%num
-      print url3
-      mark=False
-      try:
-          for j in range(100):
-              content=urllib2.urlopen(url3).read()
-              c=eval(content)
-              c=c['song']
-              for i in c:
-                  if unicode(str(i['title']),'utf-8')==handle(p1.string):
-                      urllib.urlretrieve(i['url'].replace('\\',''), fhandle(handle(p1.string))+"-"+fhandle(handle(p2.string))+".mp3")
-                      mark=True
-                      break
-              if mark:
-                  break
-          if mark:
-              print "succeed!\n"
-          else: print "fail!\n"
-      except Exception as e:
-          print e.message
+    req = urllib2.Request(myurl)
+    req.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)')
+    req.add_header('Cookie', cookie)
+    content = urllib2.urlopen(req).read()
+    soup = BeautifulSoup(str(content))
+    for div in soup.findAll('div', {'class' : 'info_wrapper'}):
+        p = div.find('div', {'class' : 'song_info'}).findAll("p")
+        sid = div.find('div', {'class' : 'action'})['sid']
+        print "song:" + html_decode(p[0].string) + "\nsinger:" + html_decode(p[1].string) + "\nalbum:" + html_decode(p[2].a.string)
+        mark = False
+        try:
+            for j in range(10):
+                songs = get_songs_information(str(p[2].a))
+                for song in songs:
+                    if sid == song['sid']:
+                        download(song)
+                        mark = True
+                        break
+                if mark:
+                    break
+            if mark:
+                print 'succeed!\n\n'
+            else: print 'fail!\n\n'
+        except Exception as e:
+            print e.message+'\n'
  
 def main():
-    url="http://douban.fm/mine?start=%d&type=liked"
-    cookie=raw_input('cookie:')
-    print "you should enter the pages you want to download"
-    page0=int(raw_input('page from:'))
-    page1=int(raw_input('page to:'))
+    url = 'http://douban.fm/mine?start=%d&type=liked'
+    cookie = raw_input('cookie:')
+    print 'you should enter the pages you want to download'
+    page0 = int(raw_input('page from:'))
+    page1 = int(raw_input('page to:'))
     for i in range(page1-page0+1):
-        get(url%((i+page0-1)*15),cookie)
+        get(url%((i+page0-1)*15), cookie)
  
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
